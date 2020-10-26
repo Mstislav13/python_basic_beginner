552eb50063ca6455551a60e99be7a145e8d4edd7
mstislav <strong13strong@gmail.com>
2020-10-26 17:36:15 +0700

    final

--- homeworks/les1/task2.py
+++ homeworks/les1/task2.py
@@ -1,3 +1,9 @@
 time = int(input('Введите время в секундах: '))
-conversion = f"{time} секунд соответствует:\n {(time/3600)} часам,\n {(time/60)} минутам,\n {time} секундам."
+hours = time // 3600
+minutes = (time - hours * 3600) // 60
+seconds = time - (hours * 3600 + minutes * 60)
+print(f"Время: {hours} часов: {minutes} минут: {seconds} секунд")
+
+# Ещё один вариант перевода:
+conversion = f"{time} секунд соответствует:\n {time/3600} часам,\n {time/60} минутам,\n {time} секундам."
 print(conversion)
